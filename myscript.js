8a4b0be3dd20337d15ebc829aa2450d2f0807b5c
Shridhi Aggarwal <shridhi.aggarwal@comprotechnologies.com>
2021-07-02 13:15:57 +0530

    image transition changes in html, css, js

--- myscript.js
+++ myscript.js
@@ -2,24 +2,26 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("bookcover").style.display = "none";
-    document.getElementById("bookcover").style.opacity = "0";
-    document.getElementById("bookname").style.fontsize = "3rem";
-    document.getElementById("bookname").style.marginTop = "7px";
-    document.getElementById("booktype").style.display = "none";
-    document.getElementById("date").style.display = "none";
+    // document.getElementById("bookcover").style.display = "none";
+    // document.getElementById("bookcover").style.opacity = "0";
     // document.getElementById("openbtn").style.display = "none";
-    document.getElementById("flex-item2").style.paddingTop = "0px";
-    document.querySelector(".flex-item3").classList.add("flex-item3-shrink");
+    document.getElementById("banner").classList.add("tiny");
+    document.getElementById("bookcover").classList.add("tiny");
+    document.getElementById("flex-item2").classList.add("tiny");
+    document.getElementById("bookname").classList.add("tiny");
+    document.getElementById("booktype").style.display = "none";
+    document.getElementById("date").classList.add("tiny");
+    document.getElementById("flex-item3").classList.add("tiny");
   } else {
-    document.getElementById("bookcover").style.display = "block";
-    document.getElementById("bookcover").style.opacity = "1";
-    document.getElementById("bookname").style.fontsize = "2rem";
-    document.getElementById("bookname").style.marginTop = "0px";
-    document.getElementById("booktype").style.display = "block";
-    document.getElementById("date").style.display = "inline-flex";
+    // document.getElementById("bookcover").style.display = "block";
+    // document.getElementById("bookcover").style.opacity = "1";
     // document.getElementById("openbtn").style.display = "inline-flex";
-    document.getElementById("flex-item2").style.paddingTop = "80px";
-    document.querySelector(".flex-item3").classList.remove("flex-item3-shrink");
+    document.getElementById("banner").classList.remove("tiny");
+    document.getElementById("bookcover").classList.remove("tiny");
+    document.getElementById("flex-item2").classList.remove("tiny");
+    document.getElementById("bookname").classList.remove("tiny");
+    document.getElementById("booktype").style.display = "block";
+    document.getElementById("date").classList.remove("tiny");
+    document.getElementById("flex-item3").classList.remove("tiny");
   }
 }
