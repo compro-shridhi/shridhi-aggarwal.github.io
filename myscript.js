6b24f1a4ec7ed795d05c27c5f9fab343667f4eab
Shridhi Aggarwal <shridhi.aggarwal@comprotechnologies.com>
2021-07-02 13:52:56 +0530

    changes for open flip display button

--- myscript.js
+++ myscript.js
@@ -7,15 +7,17 @@ function scrollFunction() {
     document.getElementById("bookname").style.fontsize = "3rem";
     document.getElementById("booktype").style.display = "none";
     document.getElementById("date").style.display = "none";
-    document.getElementById("openbtn").style.display = "none";
+    // document.getElementById("openbtn").style.display = "none";
     document.getElementById("flex-item2").style.paddingTop = "0px";
+    document.querySelector(".flex-item3").classList.add("flex-item3-shrink");
   } else {
     document.getElementById("bookcover").style.display = "block";
     document.getElementById("bookcover").style.opacity = "1";
     document.getElementById("bookname").style.fontsize = "2rem";
     document.getElementById("booktype").style.display = "block";
     document.getElementById("date").style.display = "inline-flex";
-    document.getElementById("openbtn").style.display = "inline-flex";
+    // document.getElementById("openbtn").style.display = "inline-flex";
     document.getElementById("flex-item2").style.paddingTop = "80px";
+    document.querySelector(".flex-item3").classList.remove("flex-item3-shrink");
   }
 }
